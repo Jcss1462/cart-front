e0d42e879a836e46fe06115bdb45e9dea7fcb548
jcss1462 <jcss1462@gmail.com>
2020-11-30 20:13:03 -0500

    dialogListoo

--- src/app/component/store/store.component.ts
+++ src/app/component/store/store.component.ts
@@ -10,24 +10,46 @@ import { ProductService } from 'src/app/service/product.service';
 export class StoreComponent implements OnInit {
 
   //creo un arreglo de product
-  public products:Product[]
+  public products: Product[];
 
+  public productView: Product;
   //inyecto el servicio
   constructor(public productService: ProductService) { }
 
   ngOnInit(): void {
     this.findAllEnable();
+    this.productView=null;
   }
 
-  findAllEnable():void{
+  findAllEnable(): void {
     //hago la consulta y guardo los resultados
     //envio una variable para los datos y otra para los errores
-    this.productService.findAllEnable().subscribe(data=>{
-      this.products=data;
-    },error=>{
+    this.productService.findAllEnable().subscribe(data => {
+      this.products = data;
+    }, error => {
       //si hay error, lo imprimo en la consola
       console.error(error)
     });
+  }
+
+  openView(product:Product) {
+    var modal = document.getElementById("myModal");
+    modal.style.display = "block";
+    this.productView=product;
+  }
+
+  // When the user clicks on <span> (x), close the modal
+  closeView() {
+    var modal = document.getElementById("myModal");
+    modal.style.display = "none";
+  }
+
+  // When the user clicks anywhere outside of the modal, close it
+  closeAniwhere(event) {
+    var modal = document.getElementById("myModal");
+    if (event.target == modal) {
+      modal.style.display = "none";
+    }
   }
 
 }
